abbd4bc812f79c4cefd0eba0aa3dda5e46132e38
TylerTeifke <tylerteifke@gmail.com>
2025-08-03 11:14:12 -0700

    Randomized the solution for the first level.

--- src/Level1.js
+++ src/Level1.js
@@ -10,25 +10,41 @@ import Content from './Content';
 
 const Level1 = () => {
 
+  //Will randomize what the winning order is
+  const randomizeOrder = () => {
+    const order = []
+    const suits = ["Diamond", "Club", "Spade", "Heart"]
+
+    //Will insert a random suit into the order, until the order is complete
+    while(order.length < 4){
+      let newNum = (Math.random() * suits.length)
+
+      order.push(suits.at(newNum))
+      suits.splice(newNum, 1)
+    }
+
+    return order
+  }
+
   //Will be used to navigate to other levels
   let navigate = useNavigate();
 
   //Will determine when the game has been won or not
   //const [hasWon, setHasWon] = useState(false)
   const [cards, setCards] = useState([
-    { id: 1, suit: "King of Diamonds", flipped: true, card_image: king_of_diamonds },
-    { id: 2, suit: "King of Clubs", flipped: true, card_image: king_of_clubs },
-    { id: 3, suit: "King of Spades", flipped: true, card_image: king_of_spades },
-    { id: 4, suit: "King of Hearts", flipped: false, card_image: king_of_hearts },
+    { id: 1, suit: "Diamond", flipped: true, card_image: king_of_diamonds },
+    { id: 2, suit: "Club", flipped: true, card_image: king_of_clubs },
+    { id: 3, suit: "Spade", flipped: true, card_image: king_of_spades },
+    { id: 4, suit: "Heart", flipped: false, card_image: king_of_hearts },
   ]);
   //Will be used to determine what order of cards is required to win
-  const [winningOrder] = useState([4, 2, 3, 1])
+  const [winningOrder] = useState(randomizeOrder)
 
   //Will determine if the order of the cards is the same as the winning order
   const handleCheckButtonPress = () => {
     console.log(cards)
     for(let i = 0; i < cards.length; i++){
-      if(cards[i].id !== winningOrder[i]){
+      if(cards[i].suit !== winningOrder[i]){
         return
       }
     }
@@ -40,7 +56,7 @@ const Level1 = () => {
   return (
     <div>
       <header>
-        In order to win, make the order, from top to bottom, Heart, Club, Spade, Diamond
+        In order to win, make the order, from top to bottom, { winningOrder[0] }, { winningOrder[1] }, { winningOrder[2] }, { winningOrder[3] }
       </header>
       <Content cards={cards} setCards={setCards} handleCheckButtonPress={handleCheckButtonPress}/>
     </div>
